b6fa25f65d05c642780525ea3200a95b88298366
Martin Lavoie <broemartino@gmail.com>
2018-12-04 08:24:29 -0500

    Refactor overmind as modules

---
+++ modules/base.nix
@@ -0,0 +1,25 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [ ];
+  
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+  	enable = true;
+	forwardX11 = true;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
---
+++ modules/vfio.nix
@@ -0,0 +1,19 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  boot.kernelParams = [ "amd_iommu=on" ];
+  boot.blacklistedKernelModules = [ "nvidia" "nouveau" ];
+  boot.kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+  boot.extraModprobeConfig = "options vfio-pci ids=10de:1401,10de:0fba";
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuOvmf = true;
+    };
+  };
+}
