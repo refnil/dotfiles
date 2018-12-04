180e793215860076458e79857297912b04445a83
Martin Lavoie <broemartino@gmail.com>
2018-12-04 08:53:19 -0500

    user

--- modules/machine/overmind.nix
+++ modules/machine/overmind.nix
@@ -10,6 +10,8 @@
       ../hardware/overmind.nix
       ../base.nix
       ../vfio.nix
+
+      ../user/refnil.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -17,40 +19,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
   
   networking.hostName = "overmind"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   # sound.enable = true;
@@ -61,17 +29,10 @@
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.refnil = {
-    isNormalUser = true;
-    #uid = 1000;
-  };
-
+  # Set group for sudoers
+  users.extraUsers.refnil.extraGroups = [ "wheel" ];
 }
---
+++ modules/user/refnil.nix
@@ -0,0 +1,14 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  programs.fish.enable = true;
+  users.users.refnil = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+  };
+
+}
