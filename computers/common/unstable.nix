e6374fff09e5760539ec20734c2b3f8f1132a7fc
Martin Lavoie <broemartino@gmail.com>
2019-09-26 00:34:26 -0400

    Update unstable

--- computers/common/unstable.nix
+++ computers/common/unstable.nix
@@ -5,7 +5,7 @@ let
   mkIf = self.lib.mkIf;
   unstableTarball = builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/91d748670355b4f10ff63d4c6f7cd083613a6528.tar.gz";
-      sha256 = "0mi032xs1jsak93wa7bqa6z2ingz91z0g2ncc2g9mb754mpxp6xb";
+      sha256 = "0sg6rkncfq51gcj1cjlrbjb7pd6f6dq4i5fjx7z9nillkqqdc566";
   };
 in
 {
