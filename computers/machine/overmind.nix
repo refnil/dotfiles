46becb72c3f7d13d99a869d291a118c79d83b7bf
Martin Lavoie <broemartino@gmail.com>
2020-03-25 09:45:53 -0400

    use latest kernel to fix bug

--- computers/machine/overmind.nix
+++ computers/machine/overmind.nix
@@ -34,6 +34,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   
   networking.hostName = "overmind"; # Define your hostname.
 
