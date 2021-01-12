3c55ee631a8e059caa8d851b723f2dc0fffb8a0d
Martin Lavoie <broemartino@gmail.com>
2021-01-12 22:13:15 +0100

    Update sources

--- home-folders/refnil/firefox-addons/firefox-addons-generated.nix
+++ home-folders/refnil/firefox-addons/firefox-addons-generated.nix
@@ -2,10 +2,10 @@
   {
     "tree-style-tab" = buildFirefoxXpiAddon {
       pname = "tree-style-tab";
-      version = "3.5.31";
+      version = "3.5.34";
       addonId = "treestyletab@piro.sakura.ne.jp";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3654280/tree_style_tab_-3.5.31-fx.xpi";
-      sha256 = "1122178ebf5e8b0b62fa20353d8bb7ecb243acb16856291581337c895317145d";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3664524/tree_style_tab_-3.5.34-fx.xpi";
+      sha256 = "5533ce0fd1060b7a289e666e77af8a6aa8a0c4df1e0c3ce8bf0297ff65bff066";
       meta = with stdenv.lib;
       {
         homepage = "http://piro.sakura.ne.jp/xul/_treestyletab.html.en";
