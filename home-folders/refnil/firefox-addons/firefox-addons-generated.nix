c630cd5dda5cb9ec655b72959552d3d5b9b50380
Martin Lavoie <broemartino@gmail.com>
2021-04-28 22:48:58 +0200

    update sources

--- home-folders/refnil/firefox-addons/firefox-addons-generated.nix
+++ home-folders/refnil/firefox-addons/firefox-addons-generated.nix
@@ -1,25 +1,11 @@
 { buildFirefoxXpiAddon, fetchurl, lib, stdenv }:
   {
-    "gsconnect" = buildFirefoxXpiAddon {
-      pname = "gsconnect";
-      version = "8";
-      addonId = "gsconnect@andyholmes.github.io";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3626312/gsconnect-8-fx.xpi";
-      sha256 = "68b2fe0a9e064ccf869af5a9021f4d90661b8b05ca330c39d852a6cc1aa92274";
-      meta = with lib;
-      {
-        homepage = "https://github.com/andyholmes/gnome-shell-extension-gsconnect";
-        description = "Share links with GSConnect, direct to the browser or by SMS. Requires at least v7 of the Gnome Shell extension to function.";
-        license = licenses.gpl2;
-        platforms = platforms.all;
-        };
-      };
     "tree-style-tab" = buildFirefoxXpiAddon {
       pname = "tree-style-tab";
-      version = "3.7.3";
+      version = "3.7.4";
       addonId = "treestyletab@piro.sakura.ne.jp";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3734736/tree_style_tab_-3.7.3-fx.xpi";
-      sha256 = "9b2bf3723115c6d389197afa374ac8294d95ca5e3e74a68dba5f365536cab9f6";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3752249/tree_style_tab_-3.7.4-fx.xpi";
+      sha256 = "9e7a9b1ea63c62511e915e301ec7c8f93fabb4551b908bbd4f8e719adf9336d6";
       meta = with lib;
       {
         homepage = "http://piro.sakura.ne.jp/xul/_treestyletab.html.en";
