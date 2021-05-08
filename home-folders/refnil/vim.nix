fc2f225a142383e76800571ddb4929a2443475a2
Martin Lavoie <broemartino@gmail.com>
2021-05-08 11:47:13 +0200

    Change cmdheight in vim

--- home-folders/refnil/vim.nix
+++ home-folders/refnil/vim.nix
@@ -80,7 +80,8 @@ with pkgs;
       set nowritebackup
 
       " Give more space for displaying messages.
-      set cmdheight=2
+      " Line under the statusline
+      set cmdheight=1
 
       " Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
       " delays and poor user experience.
